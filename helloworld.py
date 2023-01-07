6e08e6e1c972612e5621f0063e8ccb8d649ca12c
KarthikeyaVaitla16 <96165029+KarthikeyaVaitla16@users.noreply.github.com>
2023-01-07 15:03:21 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,34 @@
 # This program prints Hello, world!
 
 print('Hello, world!')
+trigger:
+- main
+
+pool:
+  vmImage: ubuntu-latest
+strategy:
+  matrix:
+    Python27:
+      python.version: '2.7'
+    Python35:
+      python.version: '3.5'
+    Python36:
+      python.version: '3.6'
+    Python37:
+      python.version: '3.7'
+
+steps:
+- task: UsePythonVersion@0
+  inputs:
+    versionSpec: '$(python.version)'
+  displayName: 'Use Python $(python.version)'
+
+- script: | python "helloworld.py"
+    python -m pip install --upgrade pip
+    pip install -r requirements.txt
+  displayName: 'Install dependencies'
+
+- script: |helloworld.py
+    pip install pytest pytest-azurepipelines
+    pytest
+  displayName: 'pytest'
